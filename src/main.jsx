c35b5b422a40673b2ed05e3b7f8c610ddb13029e
Ajay Patil <aj41093@gmail.com>
2024-01-26 22:11:22 +0100

    Done

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/home/home.jsx";
-import SingleProduct from "./pages/home/SingleProduct.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/home/Home.jsx';
+import SingleProduct from './pages/home/SingleProduct.jsx';
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
     children: [
       {
-        path: "/",
-        element: <Home/>
+        path: '/',
+        element: <Home />,
       },
       {
-        path: "/shop/:id",
-        element: <SingleProduct/>
-      }
-    ]
+        path: '/shop/:id',
+        element: <SingleProduct />,
+      },
+    ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
 );
